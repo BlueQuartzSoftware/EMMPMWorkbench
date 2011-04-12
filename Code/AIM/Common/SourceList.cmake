9f72c6b30e066b449bee48d7904ec7a8e17cc36d
Mike Jackson <mike.jackson@bluequartz.net>
2011-04-12 16:53:36 -0400

    Updating license and fixing copyright text

--- Code/AIM/Common/SourceList.cmake
+++ Code/AIM/Common/SourceList.cmake
@@ -1,4 +1,9 @@
-
+#--////////////////////////////////////////////////////////////////////////////
+#-- Copyright (c) 2009, Michael A. Jackson. BlueQuartz Software
+#-- All rights reserved.
+#-- BSD License: http://www.opensource.org/licenses/bsd-license.html
+#-- This code was partly written under US Air Force Contract FA8650-07-D-5800
+#--////////////////////////////////////////////////////////////////////////////
 set (AIM_Common_SRCS
     ${PROJECT_CODE_DIR}/AIM/Common/AIMImage.cpp
 )
--- License/License.cmake
+++ License/License.cmake
@@ -1,3 +1,9 @@
+#--////////////////////////////////////////////////////////////////////////////
+#-- Copyright (c) 2009, Michael A. Jackson. BlueQuartz Software
+#-- All rights reserved.
+#-- BSD License: http://www.opensource.org/licenses/bsd-license.html
+#-- This code was partly written under US Air Force Contract FA8650-07-D-5800
+#--////////////////////////////////////////////////////////////////////////////
 # --------------------------------------------------------------------
 # create Resource files for the various license files that are used and 
 # also create a header file that lists all the License Files
