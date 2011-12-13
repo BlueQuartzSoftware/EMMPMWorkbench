8ab6452a11d4654dd963f0b261e475bebd202f58
Mike Jackson <mike.jackson@bluequartz.net>
2011-12-13 17:10:41 -0500

    Updating build script

--- IPHelperSDK.sh
+++ IPHelperSDK.sh
@@ -18,13 +18,13 @@
 # You can simply 'source "initvars.sh" any time to get the exports.
 
 # If you want to force another directory then change where "SDK_ROOT" points to.
-SDK_ROOT=/home/mjackson
+SDK_ROOT=/tmp/mjackson
 SDK_SOURCE=$SDK_ROOT/Workspace
 SDK_INSTALL=$SDK_ROOT/Toolkits
 
 # If you are on a Multicore system and want to build faster set the variable to the number
 # of cores/CPUs available to you.
-PARALLEL_BUILD=2
+PARALLEL_BUILD=16
 
 HOST_SYSTEM=`uname`
 
@@ -33,7 +33,7 @@ HOST_SYSTEM=`uname`
 BUILD_CMAKE="1"
 BUILD_MXABOOST="1"
 BUILD_QWT="1"
-BUILD_ITK="1"
+BUILD_ITK="0"
 BUILD_TIFF="1"
 BUILD_EMMPM="1"
 BUILD_IPHELPER="1"
@@ -66,8 +66,8 @@ if [ "$WGET" == "" ]
 fi
 
 # Create some directories for our use
-mkdir $SDK_SOURCE
-mkdir $SDK_INSTALL
+mkdir -p $SDK_SOURCE
+mkdir -p $SDK_INSTALL
 cd $SDK_SOURCE
 
 DOWNLOAD_PROG=""
@@ -79,7 +79,7 @@ fi
 
 if [ "$CURL" != "" ]; then
   DOWNLOAD_PROG=$CURL
-  DOWNLOAD_ARGS="-o cmake-2.8.3.tar.gz"
+  DOWNLOAD_ARGS="-o cmake-2.8.6.tar.gz"
 fi
 
 if [ "$BUILD_CMAKE" == "1" ]
@@ -88,18 +88,18 @@ then
 
 
 #Download and Compile CMake
-$DOWNLOAD_PROG "http://www.cmake.org/files/v2.8/cmake-2.8.3.tar.gz" $DOWNLOAD_ARGS
-tar -xvzf cmake-2.8.3.tar.gz
-cd cmake-2.8.3
-./configure --prefix=$SDK_INSTALL/cmake-2.8.3
-make -j $PARALLEL_BUILD install
+# $DOWNLOAD_PROG "http://www.cmake.org/files/v2.8/cmake-2.8.6.tar.gz" $DOWNLOAD_ARGS
+# tar -xvzf cmake-2.8.6.tar.gz
+# cd cmake-2.8.6
+# ./configure --prefix=$SDK_INSTALL/cmake-2.8.6 --parallel $PARALLEL_BUILD
+# make -j $PARALLEL_BUILD install
 
 # Export these variables for our use and then echo them into a file that people can use
 # to setup their environment
-export CMAKE_INSTALL=$SDK_INSTALL/cmake-2.8.3
+export CMAKE_INSTALL=$SDK_INSTALL/cmake-2.8.6
 export PATH=$CMAKE_INSTALL/bin:$PATH
 
-echo "export CMAKE_INSTALL=$SDK_INSTALL/cmake-2.8.3" >  $SDK_INSTALL/initvars.sh
+echo "export CMAKE_INSTALL=$SDK_INSTALL/cmake-2.8.6" >  $SDK_INSTALL/initvars.sh
 echo "export PATH=\$CMAKE_INSTALL/bin:\$PATH" >>  $SDK_INSTALL/initvars.sh
 fi
 
@@ -109,10 +109,10 @@ then
 # We now need MXABoost on the system
 cd $SDK_SOURCE
 # Remove any previous MXABoost
-rm -rf MXABoost
-git clone git://scm.bluequartz.net/support-libraries/mxaboost.git MXABoost
+#rm -rf MXABoost
+git clone git://scm.bluequartz.net/MXABoost.git MXABoost
 cd MXABoost
-git checkout boost-1.44
+git checkout master
 mkdir Build
 cd Build
 cmake -DBOOST_INCLUDE_INSTALL_DIR=include/boost-1_44 -DBOOST_LIB_INSTALL_DIR=lib -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/MXABoost-1.44 -DENABLE_SHARED=OFF -DWINMANGLE_LIBNAMES=ON ../
@@ -128,8 +128,8 @@ then
 #------------------------------------------------------------------------------
 cd $SDK_SOURCE
 # Remove any previous Qwt
-rm -rf Qwt
-git clone --recursive git://scm.bluequartz.net/support-libraries/qwt.git Qwt
+#rm -rf Qwt
+git clone --recursive git://scm.bluequartz.net/Qwt.git Qwt
 cd Qwt
 mkdir Build
 cd Build
@@ -177,8 +177,8 @@ then
 # Compile libTiff
 cd $SDK_SOURCE
 # Remove any previous tiff
-rm -rf tiff
-git clone --recursive git://scm.bluequartz.net/support-libraries/tiff.git tiff
+#rm -rf tiff
+git clone --recursive git://scm.bluequartz.net/Tiff.git tiff
 cd tiff
 mkdir Build
 cd Build
@@ -208,9 +208,9 @@ then
 # Compile the emmpm Library
 cd $SDK_SOURCE
 # Remove any previous emmpm
-rm -rf emmpm
-git clone --recursive git://scm.bluequartz.net/eim/emmpm.git emmpm
-cd emmpm
+#rm -rf EMMPMGui
+git clone --recursive git://scm.bluequartz.net/EMMPMGui.git
+cd EMMPMGui
 mkdir Build
 cd Build
 # On OS X we need to set the "install_name" correctly on Libraries that will get used which is
@@ -219,33 +219,17 @@ if ["$HOST_SYSTEM" == "Darwin" ]
   then
   ADDITIONAL_ARGS="-DCMP_BUILD_WITH_INSTALL_NAME=ON"
 fi
-cmake $ADDITIONAL_ARGS -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/emmpm -DBUILD_SHARED_LIBS=ON ../
+cmake $ADDITIONAL_ARGS -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/EMMPMGui -DBUILD_SHARED_LIBS=ON ../
 make -j $PARALLEL_BUILD
 make install
 cd ../
 mkdir zRel
 cd zRel
-cmake $ADDITIONAL_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/emmpm -DBUILD_SHARED_LIBS=ON ../
+cmake $ADDITIONAL_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/EMMPMGui -DBUILD_SHARED_LIBS=ON ../
 make -j $PARALLEL_BUILD
 make install
-export EMMPM_INSTALL=$SDK_INSTALL/emmpm
-echo "export EMMPM_INSTALL=$SDK_INSTALL/emmpm" >> $SDK_INSTALL/initvars.sh
+#export EMMPM_INSTALL=$SDK_INSTALL/emmpm
+#echo "export EMMPM_INSTALL=$SDK_INSTALL/emmpm" >> $SDK_INSTALL/initvars.sh
 fi
-
-
-#------------------------------------------------------------------------------
-# Compile the IPHelper
-# Pull Down the IPHelper Source and compile it
-cd $SDK_SOURCE
-# Remove any previous IPHelper
-#rm -rf IPHelper
-#git clone git://scm.bluequartz.net/eim/iphelper.git IPHelper
-cd IPHelper
-git checkout user_init
-mkdir Build
-cd Build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$SDK_INSTALL/IPHelper  ../
-make -j $PARALLEL_BUILD 
-
 
 
